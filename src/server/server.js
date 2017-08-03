dda87b924159fe7cdd2b1bbf14cf87e83d7a77e8
Yusinto Ngadiman <yus@Yusintos-MacBook-Pro.local>
2017-08-03 18:08:15 +1000

    Added express wasm middleware. Beginning to connect wasm function to javascript. TODO: onload wasm function is undefined!

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,10 @@
+import path from 'path';
+import fs from 'fs';
 import Express from 'express';
 import Webpack from 'webpack';
 import WebpackConfig from '../../webpack.config';
 import WebpackDevMiddleware from 'webpack-dev-middleware';
 import WebPackHotMiddleware from 'webpack-hot-middleware';
-
-import RelayEnvironment from '../universal/relayEnvironment';
-import App, {appRootQuery} from '../universal/app';
-import React from 'react';
-import {renderToStaticMarkup} from 'react-dom/server';
 
 const app = Express();
 const webpackCompiler = Webpack(WebpackConfig);
@@ -17,73 +14,41 @@ app.use(WebpackDevMiddleware(webpackCompiler, {
   quiet: false
 }));
 app.use(WebPackHotMiddleware(webpackCompiler));
-app.use((req, res) => {
 
-  // YAYY!!! This works!
-  /**
-   * Returns
-   * {
-  "fieldPayloads": [],
-  "source": {
-    "client:root": {
-      "__id": "client:root",
-      "__typename": "__Root",
-      "viewer": {
-        "__ref": "viewer-fixed"
-      }
-    },
-    "viewer-fixed": {
-      "__id": "viewer-fixed",
-      "__typename": "Viewer",
-      "id": "viewer-fixed",
-      "Customer{\"id\":\"cj3xrfkrapzo90174udonmzsh\"}": {
-        "__ref": "cj3xrfkrapzo90174udonmzsh"
-      }
-    },
-    "cj3xrfkrapzo90174udonmzsh": {
-      "__id": "cj3xrfkrapzo90174udonmzsh",
-      "__typename": "Customer",
-      "email": "yusinto@gmail.com",
-      "id": "cj3xrfkrapzo90174udonmzsh"
+app.get('/:filename.wasm', (req, res) => {
+  const wasmFilePath = path.resolve(__dirname, `../../dist/${req.params.filename}.wasm`);
+  console.log(`Wasm request ${wasmFilePath}`);
+
+  fs.readFile(wasmFilePath, (err, data) => {
+    const errorMessage = `Error ${wasmFilePath} not found. ${JSON.stringify(err)}`;
+    if (err) {
+      console.log(errorMessage);
+      res.status(404).send(errorMessage);
+      return;
     }
-  }
-}
-   */
-  const {createOperationSelector} = RelayEnvironment.unstable_internal;
-  const operation = createOperationSelector(appRootQuery());
-  // console.log(`appRootQuery look like: ${JSON.stringify(appRootQuery)}`);
-  // console.log(`operation look like: ${JSON.stringify(operation)}`);
-  RelayEnvironment.sendQuery({
-    cacheConfig: null,
-    onCompleted: () => console.log('onCompleted'),
-    onError: () => console.log('onError'),
-    onNext: (results) => {
+    res.send(data);
+  });
+});
+app.use('/dist', Express.static('dist', {maxAge: '1d'}));
 
-      // TODO: at the moment <App /> outputs <div>loading...</div> which needs to be fixed!
-
-      // At this point, the graphql response should already be in the Store
-      // So we can look it up and use it to render our App component
-      const snapshot = RelayEnvironment.lookup(operation.fragment);
-
-      // TODO: Somehow supply snapshot.data to <App />
-      const htmlString = `<!DOCTYPE html>
+app.use((req, res) => {
+  const htmlString = `<!DOCTYPE html>
     <html>
          <head>
-            <title>Relay Modern SSR</title>
+            <title>Hasta la vista JS!</title>
             <meta name="viewport" content="width=device-width, initial-scale=1">
           </head>
           <body>
-            <div id="reactDiv">${renderToStaticMarkup(<App data={snapshot.data}/>)}</div>
+            <div id="reactDiv"/>
+            <script src="/dist/helloWorld.js"></script>
             <script src="/dist/bundle.js"></script>
           </body>
     </html>`;
 
-      res.end(htmlString);
-    },
-    operation,
-  });
+  res.end(htmlString);
 });
 
 app.listen(3000, () => {
-  console.log(`Started relay-modern-ssr...`);
+  console.log(`Started wasm-playground...`);
 });
+
--- src/universal/app.js
+++ src/universal/app.js
@@ -1,33 +1,11 @@
 import React, {Component} from 'react';
-import Customer from './customer';
-import {QueryRenderer, graphql} from 'react-relay';
-import environment from './relayEnvironment';
-
-export const appRootQuery = graphql`
-  query appRootQuery {
-    viewer {
-      ...customer_viewer
-    }
-  }
-`;
-
-const renderMethod = ({error, props}, data) => {
-  if(data) {
-    return <Customer viewer={data.viewer}/>;
-  }
-
-  if (error) {
-    return <div>Error! {error.message}</div>;
-  } else if (props) {
-    return <Customer viewer={props.viewer}/>;
-  }
-  return <div>loading...</div>;
-};
 
 class App extends Component {
   render() {
-    console.log(`data looks like: ${JSON.stringify(this.props.data)}`);
-    return <QueryRenderer environment={environment} query={appRootQuery} render={(a) => renderMethod(a, this.props.data)}/>;
+    let diceResult = -1;
+    if(typeof _roll_dice !== 'undefined') diceResult = _roll_dice();
+
+    return <div>Welcome to WebAsm! Dice: {diceResult}</div>;
   }
 }
 
