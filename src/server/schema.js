a5f78c1ba091669dd90887a58dde1a6b7188cc2e
Yus <yusinto@gmail.com>
2018-01-11 15:49:22 +1100

    a

--- src/server/schema.js
+++ src/server/schema.js
@@ -1,16 +1,30 @@
-import {GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt} from 'graphql';
+import {GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList} from 'graphql';
+
+const countries = [
+  {
+    "name": "Australia",
+    "population": 25000000,
+  },
+  {
+    "name": "New Zealand",
+    "population": 4700000,
+  }
+];
+
+const countryType = new GraphQLObjectType({
+  name: 'Country',
+  fields: {
+    name: {type: GraphQLString},
+    population: {type: GraphQLInt}
+  }
+});
 
 const query = new GraphQLObjectType({
   name: 'Root',
   fields: {
     country: {
-      type: new GraphQLObjectType({
-        name: 'Country',
-        fields: {
-          name: {type: GraphQLString},
-          population: {type: GraphQLInt}
-        }
-      })
+      type: new GraphQLList(countryType),
+      resolve: () => countries,
     }
   }
 });
